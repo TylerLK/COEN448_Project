ed2be90a537a463f22447d282f67ab52c7be4869
Sunil Kublalsingh <hulksunil@hotmail.com>
2026-01-30 21:48:25 -0500

    added robot class + enums. Will probably need to edit later to include a toString or something

---
+++ COEN448_Project/src/main/java/com/robot/COEN448_Project/enums/Orientation.java
@@ -0,0 +1,16 @@
+package com.robot.COEN448_Project.enums;
+
+/*
+ * Orientation Enum
+ * @author Sunil
+ * @NORTH The robot is facing North.
+ * @EAST The robot is facing East.
+ * @SOUTH The robot is facing South.
+ * @WEST The robot is facing West.
+ */
+public enum Orientation {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
